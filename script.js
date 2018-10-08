ab19ba7747452d5913ecc389c61230949dabaf98
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-08 13:39:37 +0200

    add JS version

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-$('.article button').on('click', function () {
-	$('.modal-wrap').addClass('active');
-	$('.article').addClass('blur');
-})
-$('span.hide').on('click', function () {
-	$('.modal-wrap').removeClass('active');
-	$('.article').removeClass('blur');
-})
+// $('.article button').on('click', function () {
+// 	$('.modal-wrap').addClass('active');
+// 	$('.article').addClass('blur');
+// })
+// $('span.hide').on('click', function () {
+// 	$('.modal-wrap').removeClass('active');
+// 	$('.article').removeClass('blur');
+// })
 //short verison
 // $('.article button, span.hide').on('click', function () {
 // 	$('.modal-wrap').toggleClass('active');
 // 	$('.article').toggleClass('blur');
 // })
+
+//JS version 
+const btnModal = document.querySelector('.article button');
+btnModal.addEventListener('click', function () {
+	document.querySelector('.modal-wrap').classList.add('active');
+	document.querySelector('.article').classList.add('blur');
+}) 
+document.querySelector('span.hide').addEventListener('click', function () {
+	document.querySelector('.modal-wrap').classList.remove('active');
+	document.querySelector('.article').classList.remove('blur');
+}) 
