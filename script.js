1b0bcafa68a046536c205adf373319e00bcce5fe
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-08 13:24:06 +0200

    add short version in jQ

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ $('span.hide').on('click', function () {
 	$('.modal-wrap').removeClass('active');
 	$('.article').removeClass('blur');
 })
+//short verison
+// $('.article button, span.hide').on('click', function () {
+// 	$('.modal-wrap').toggleClass('active');
+// 	$('.article').toggleClass('blur');
+// })
